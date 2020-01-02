fe5a0cdfc666acdee4e96a836152638e3b63ca3e
VanessaKnox <knox.vanessa.a@gmail.com>
2020-01-01 21:08:53 -0500

    data test 3

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,120 +1,54 @@
-var svgWidth = 960;
-var svgHeight = 500;
+<!DOCTYPE html>
+<html lang="en">
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
-};
+<head>
+  <meta charset="UTF-8">
+  <title>D3Times</title>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/d3Style.css">
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+</head>
 
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3.select("#scatter")
-            .append("svg")
-            .attr("width", svgWidth)
-            .attr("height", svgHeight);
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <h1>D3Times</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12  col-md-9">
+        <div id="scatter">
+          <!-- We append our chart here. -->
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12  col-md-9">
+        <div class="article">
+          <h2>Correlations Discovered Between Health Risks and Age, Income</h2>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 
-// Append an SVG group
-var chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+  <!-- Footer-->
+  <div id="footer">
+  
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.5.0/d3.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3-tip/0.9.1/d3-tip.js"></script>
+  <script type="text/javascript" src="assets/js/app.js"></script>
 
-d3.csv("assets/js/data.csv")
-    .then(function(healthData) {
+</body>
 
-        healthData.forEach(function(d) {
-            d.poverty = +d.poverty;
-            d.healthcare = +d.healthcare;
-        });
-
-        console.log(healthData);
-
-        // Configure axes
-        var xMin = d3.min(healthData, function(d) {
-            return d.poverty;
-        });
-
-        var xMax = d3.max(healthData, function(d) {
-            return d.poverty;
-        });
-
-        var yMin = d3.min(healthData, function(d) {
-            return d.healthcare;
-        });
-
-        var yMax = d3.max(healthData, function(d) {
-            return d.healthcare;
-        });
-
-        console.log(xMin);
-        console.log(xMax);
-        console.log(yMin);
-        console.log(yMax);
-
-        var labelArea = 30
-
-        var xLinearScale = d3.scaleLinear().domain([(xMin - .5), xMax + 2]).range([0, width]);
-        var yLinearScale = d3.scaleLinear().domain([(yMin - 1), yMax + 2]).range([height, 0]);
-
-        var bottomAxis = d3.axisBottom(xLinearScale);
-        var leftAxis = d3.axisLeft(yLinearScale);
-
-        // Append axes
-        chartGroup.append("g")
-                  .attr("transform", `translate(0, ${height})`)
-                  .call(bottomAxis);
-
-        chartGroup.append("g").call(leftAxis);
-
-        // Circles
-        chartGroup.selectAll("circle")
-                  .data(healthData)
-                  .enter()
-                  .append("circle")
-                  .attr("cx", function(data) {
-                      return xLinearScale(data.poverty)
-                  })
-                  .attr("cy", function(data) {
-                      return yLinearScale(data.healthcare)
-                  })
-                  .attr("r", "15")
-                  .attr("fill", "mediumturquoise")
-                  .attr("opacity", .7);
-
-        // Abbreviations in Circles
-        chartGroup.append("text")
-            .style("text-anchor", "middle")
-            .style("font-size", "12px")
-            .selectAll("tspan")
-            .data(healthData)
-            .enter()
-            .append("tspan")
-            .attr("x", function(data) {
-                return xLinearScale(data.poverty - 0);
-                })
-            .attr("y", function(data) {
-                return yLinearScale(data.healthcare - 0.2);
-                })
-            .text(function(data) {
-                return data.abbr
-                });
-
-        // Axis Labels
-        chartGroup.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0-margin.left + 40)
-            .attr("x", 0 - height/1.5)
-            .attr("dy","1em")
-            .attr("class", "axis-text")
-            .text("Lacks Healthcare (%)");
-
-        chartGroup.append("text")
-            .attr("transform", "translate(" + width / 2.25 + " ," + (height + margin.top + 30) + ")")
-            .attr("class", "axis-text")
-            .text("In Poverty (%)");
-        })
-        
-    .catch(function(error){
-        })
+</html>
